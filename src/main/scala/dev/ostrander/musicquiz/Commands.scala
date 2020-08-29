b21cb9cd14a85fd7d095eece1e97fffd0a3774db
Jonathan Ostrander <jonathanost@gmail.com>
2020-08-29 17:42:49 -0400

    add reset command (#13)

--- src/main/scala/dev/ostrander/musicquiz/Commands.scala
+++ src/main/scala/dev/ostrander/musicquiz/Commands.scala
@@ -7,17 +7,28 @@ import ackcord.requests.Requests
 import akka.actor.typed.ActorRef
 import dev.ostrander.musicquiz.actor.GameManager
 
-class Commands(
-  requests: Requests,
-  gameActor: ActorRef[GameManager.Command],
-) extends CommandController(requests) {
-  val game: NamedCommand[RemainingAsString] =
-    GuildVoiceCommand.named(Seq("!"), Seq("start"), mustMention = false).parsing[RemainingAsString].withSideEffects { r =>
-      gameActor ! GameManager.CreateGame(r.textChannel, r.voiceChannel)
-    }
+trait Commands {
+  def apply(): List[NamedCommand[RemainingAsString]]
+}
 
-  val reset: NamedCommand[RemainingAsString] =
-    GuildCommand.named(Seq("!"), Seq("reset"), mustMention = false).parsing[RemainingAsString].withSideEffects { r =>
-      gameActor ! GameManager.Reset(r.textChannel)
+object Commands {
+  def apply(
+    requests: Requests,
+    gameActor: ActorRef[GameManager.Command],
+  ): List[NamedCommand[RemainingAsString]] = {
+    val controller: Commands = new CommandController(requests) with Commands {
+      val game: NamedCommand[RemainingAsString] =
+        GuildVoiceCommand.named(Seq("!"), Seq("start"), mustMention = false).parsing[RemainingAsString].withSideEffects { r =>
+          gameActor ! GameManager.CreateGame(r.textChannel, r.voiceChannel)
+        }
+
+      val reset: NamedCommand[RemainingAsString] =
+        GuildCommand.named(Seq("!"), Seq("reset"), mustMention = false).parsing[RemainingAsString].withSideEffects { r =>
+          gameActor ! GameManager.Reset(r.textChannel)
+        }
+
+      override def apply(): List[NamedCommand[RemainingAsString]] = game :: reset :: Nil
     }
+    controller()
+  }
 }
